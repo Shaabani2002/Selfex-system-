b556fbeb83681af7b5793ef67a6511aa2c78534c
Shaabani2002 <shabanimaulidimisigaro490@gmail.com>
2025-11-27 16:15:45 +0300

    Create File 2.server.js

---
+++ File 2.server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// CONNECT DB
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+// SAMPLE ROUTE
+app.get("/", (req, res) => {
+  res.send("BetBlock API is running...");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
